8d11cf4d51fa3d9352db52ead255ff7e0011932f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-11 14:22:06 +0500

    updated

---
+++ Experiments.sh
@@ -0,0 +1,7 @@
+# pdf book me sy excesises ko screenshot jupyter notebook me dalna tha:
+# 1:
+alias si='xclip -selection clipboard -t image/png -o >'
+# ab har dafa screenshoot lya, or phir number(sequence me) enter kya
+read a; si image_$a.png
+# ooper wali line sy user input mangy ga, to agar me <4> press karun ga to clipboard par mojud screen shot directory me image_4.png k name sy save ho jay ga.
+
