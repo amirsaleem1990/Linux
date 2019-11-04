f16f1b153227c28268b9861d4c7109fe6bfc774a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 12:44:24 +0500

    updated

--- Learning/prompt_2.sh
+++ Learning/prompt_2.sh
@@ -4,20 +4,20 @@
 # Exercise_7 - Modify the previous script to that it accepts the file or directory name as an argument instead of prompting the user to enter it.
 
 
-b=$(ls -l | grep -w $@)
+b=$(ls -l | grep -w $1)
 
 first_char=${b:0:1} # get first character of long listing for our file/folder
 
 if [[ $first_char  == "-" ]]
 	then
-		echo "<$@> is regular file"
+		echo "<$1> is regular file"
 elif [[ $first_char == "d" ]]
 	then
-		echo "<$@> is regular directory"
+		echo "<$1> is regular directory"
 
 else
-	echo "<$@> is another type of file"
+	echo "<$1> is another type of file"
 fi
 
 echo ""
-echo $(ls -l | grep -w $@)
+echo $(ls -l | grep -w $1)
