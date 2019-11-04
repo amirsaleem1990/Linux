f8c78d943211d95627b9c113adacd7d999a2e8d0
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 12:20:49 +0500

    updated

--- Learning/prompt.sh
+++ Learning/prompt.sh
@@ -4,5 +4,5 @@
 # Exercise_6 - write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file. Also perform an ls command against the file or directory with the long listing option.
 
 read -p "Enter file/directory name: " name
-b=ls -l $name
+b=$(ls -l $name)
 echo $b
