d417a12773595280a41807e2a29d157c7039abbd
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 13:18:55 +0500

    updated

---
+++ Learning/function_2.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# https://medium.com/@sankad_19852/shell-scripting-exercises-5eb7220c2252
+
+# Exercise_13 - Modify the script from the previous exercise. Make the “file_count” function accept a directory as an argument. Next, have the function display the name of the directory followed by a colon. Finally display the number of files to the screen on the next line. Call the function three times. First on the “/etc” directory, next on the “/var” directory and finally on the “/usr/bin” directory.
+
+
