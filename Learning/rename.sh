91c8bb5949c3d3660fe18e88e8c87933d99ade6e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 13:47:31 +0500

    updated

--- Learning/rename.sh
+++ Learning/rename.sh
@@ -4,6 +4,13 @@
 
 # Exercise_14 - Write the shell script that renames all files in the current directory that end in “.jpg” to begin with today’s date in the following format: YYYY-MM-DD. For example, if a picture of my cat was in the current directory and today was October 31,2016 it would change name from “mycat.jpg” to “2016–10–31-mycat.jpg”.
 
-JPG=ls *.jpg
-current_date=$(date '+%y-%m-%d')
-new_name="$current_date-$JPG"
+JPG=$(ls *.jpg)
+if [[ JPG == "" ]]
+	echo Sorry, There is no .jpg file
+	exit 0
+else
+	for jpg in $JPG
+		do
+			current_date=$(date '+%y-%m-%d')
+			new_name="$current_date-$JPG"
+fi
