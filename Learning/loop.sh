c1b181d9afc365d3ddeab85d04e2ba476e77392e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 12:05:42 +0500

    updated

--- Learning/loop.sh
+++ Learning/loop.sh
@@ -2,7 +2,13 @@
 # https://medium.com/@sankad_19852/shell-scripting-exercises-5eb7220c2252
 # Exercise_5 - Write a shell script that displays “man”,”bear”,”pig”,”dog”,”cat”,and “sheep” on the screen with each appearing on a separate line. Try to do this in as few lines as possible.
 
+# strings=(man bear pig dog cat sheep)
+# for i in "${strings[@]}"; do
+# 	echo $i
+# done
+
+
 strings="man bear pig dog cat sheep"
-for string in "${strings[@]}"; do
+for string in strings; do
 	echo $string
 done
