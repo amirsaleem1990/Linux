3315e1cc647f11d2c1188213854a369232f08d2e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 12:51:21 +0500

    updated

---
+++ Learning/prompt_3.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# https://medium.com/@sankad_19852/shell-scripting-exercises-5eb7220c2252
+# Exercise_8 - Modify the previous script to accept an unlimited number of files and directories as arguments.
