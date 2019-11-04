a10c42048ca5df581fa2cd73993d3b28511135aa
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 13:03:33 +0500

    updated

--- Learning/exis_0.sh
+++ Learning/exis_0.sh
@@ -2,4 +2,13 @@
 
 # https://medium.com/@sankad_19852/shell-scripting-exercises-5eb7220c2252
 
-# Exercise_11 - Write a script that executes the command “cat/etc/shadow”. If the command return a 0 exit status, report “command succeeded” and exit with a 0 exit status. If the command returns a non-zero exit status, report “Command failed” and exit with a 1 exit status.
+# Exercise_11 - Write a script that executes the command “cat /etc/shadow”. If the command return a 0 exit status, report “command succeeded” and exit with a 0 exit status. If the command returns a non-zero exit status, report “Command failed” and exit with a 1 exit status.
+
+retrn=$(cat /etc/shadow)
+
+if [[ $? == 1 ]]
+	then
+		echo "command succeeded"
+else
+	echo "command failed"
+fi
