8d87e11ae7f48796a442f365be5dde27fc267782
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 11:51:34 +0500

    updated

--- Learning/if-else.sh
+++ Learning/if-else.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-FILE = /home/amir/save_script.sh
+# Exercise_4 - Write a shell script to check to see if the file </home/amir/save_script.sh>  exists. If it does exist, display </home/amir/save_script.sh exist>. 
+# Next, check to see if you can write to the file. If you can, display <You have permissions to execute /home/amir/save_script.sh> .If you cannot, display <You do NOT have permissions to execute /home/amir/save_script.sh> 
+
+FILE=/home/amir/save_script.sh
 if [-e $FILE]
 	then
 		echo "$FILE exist"
