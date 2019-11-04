d1e914ed14445a5172af1a933da7026fde051add
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 12:49:28 +0500

    updated

--- Learning/prompt_2.sh
+++ Learning/prompt_2.sh
@@ -10,17 +10,18 @@ first_char=${b:0:1} # get first character of long listing for our file/folder
 if [[ $first_char == "" ]]
 	then 
 		echo "Sorry, file/folder not exist"
+
+else
+	if [[ $first_char  == "-" ]]
+		then
+			echo "<$1> is regular file"
+	elif [[ $first_char == "d" ]]
+		then
+			echo "<$1> is regular directory"
+
+	else
+		echo "<$1> is another type of file"
+	fi
 fi
-# if [[ $first_char  == "-" ]]
-# 	then
-# 		echo "<$1> is regular file"
-# elif [[ $first_char == "d" ]]
-# 	then
-# 		echo "<$1> is regular directory"
-
-# else
-# 	echo "<$1> is another type of file"
-# fi
-
-# echo ""
-# echo $(ls -l | grep -w $1)
+	
+echo $(ls -l | grep -w $1)
