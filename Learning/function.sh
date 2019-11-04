b29f7f0f824ad30a251913489cd93db69efa348d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 13:14:13 +0500

    updated

--- Learning/function.sh
+++ Learning/function.sh
@@ -3,3 +3,8 @@
 
 # Exercise_12 - Write a shell script that consists of a function that displays the number of files in the present working directory. Name this function “file_count” and call it in your script. If you use variable in your function, remember to make it a local variable.
 
+function file_count(){
+	files_count=$(ls | wc)
+	echo "Count of files in Working Directory: $files_count"
+	exit 0
+}
