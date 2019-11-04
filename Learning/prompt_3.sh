b3f72f829292b172848cd4ad20e84ec090429e8c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 12:52:28 +0500

    updated

--- Learning/prompt_3.sh
+++ Learning/prompt_3.sh
@@ -2,3 +2,28 @@
 
 # https://medium.com/@sankad_19852/shell-scripting-exercises-5eb7220c2252
 # Exercise_8 - Modify the previous script to accept an unlimited number of files and directories as arguments.
+
+files=$@
+for file in $files
+	do 
+		b=$(ls -l | grep -w $file)
+
+		first_char=${b:0:1} # get first character of long listing for our file/folder
+		if [[ $first_char == "" ]]
+			then 
+				echo "Sorry, file/folder not exist"
+
+		else
+			if [[ $first_char  == "-" ]]
+				then
+					echo "<$file> is regular file"
+			elif [[ $first_char == "d" ]]
+				then
+					echo "<$file> is regular directory"
+
+			else
+				echo "<$file> is another type of file"
+			fi
+		fi
+			
+		echo $(ls -l | grep -w $file)
