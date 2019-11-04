88bcebe759bf3cd4c20b2da0ba05a293ff6dd96a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-04 15:17:35 +0500

    updated

--- Learning/rename-2.sh
+++ Learning/rename-2.sh
@@ -3,18 +3,18 @@
 # Exercise_15 - Write the script that renames files based on the file extension. Next,It should ask the user what prefix to prepend to the file name(s). By default, the prefix should be the current date in YYYY-MM-DD format. If the user simply press enter,the current date will be used. Otherwise,whatever the user entered will be used as the prefix. Next,it should display the original file name and new name of the file. Finally,it should rename the file.
 
 
-# read -p "Enter Directory name (default is Current directory): " directory
-# if [[ $directory == "" ]]
-# 	then
-# 		directory=$(pwd)
-# fi
+read -p "Enter Directory name (default is Current directory): " directory
+if [[ $directory == "" ]]
+	then
+		directory=$(pwd)
+fi
 
-# cd $directory
+cd $directory
 
-# echo -e "\nYou have these extensions in this directory"
-# for f in *.*; do printf "%s\n" "${f##*.}"; done | sort -u
+echo -e "\nYou have these extensions in this directory"
+for f in *.*; do printf "%s\n" "${f##*.}"; done | sort -u
 
-# read -p "Enter file extension (eg: png) : " extension
+read -p "Enter file extension (eg: png) : " extension
 
 
 read -p "Enter prefix (default is current date) : " prefix 
