8dbfd8f3008205f1647f6f19b0d9412dcc430686
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-30 13:29:35 +0500

    updated

--- fill-drive-with-zeros-using-dd.sh
+++ fill-drive-with-zeros-using-dd.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-read -p "Enter File size in GB: " file_size
-read -p "Enter place to create a <zeros> file: " place
-dd if=/dev/zero of=$place bs=512 count=$(expr $file_size \* 1024 \* 1024 \* 1024 / 512)
+# https://how-to.fandom.com/wiki/How_to_wipe_a_hard_drive_clean_in_Linux
+# Wiping the entire disk
+# This will overwrite all partitions, master boot records, and data. Use the sudo command as well (sudo dd...)
+    # Filling the disk with all zeros (This may take a while, as it is making every bit of data 0) :
+    dd if=/dev/zero of=/dev/sdX bs=1M
+# If you are wiping your hard drive for security, you should populate it with random data rather than zeros (This is going to take even longer than the first example.) :
+    dd if=/dev/urandom of=/dev/sdX bs=1M
+
