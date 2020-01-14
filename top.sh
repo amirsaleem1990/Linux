f2a9309d43ee30bbcf98a79a30bb347cf2332595
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-01-14 09:51:34 +0500

    updated

--- top.sh
+++ top.sh
@@ -1,1 +1,1 @@
-for i in {1..10} ; do top -n 1 | grep "amir\|root" | sed 's/\ \{1,\}/,/g' | cut -d "," -f13 > temp.txt; done ; cat temp.txt | uniq | sort ; del -rf temp.txt
+del -rf temp.txt; for i in {1..10} ; do top -n 1 | grep "amir\|root" | sed 's/\ \{1,\}/,/g' | cut -d "," -f13 >> temp.txt ;sleep 1; done ; cat temp.txt | sort | uniq  ; del -rf temp.txt
