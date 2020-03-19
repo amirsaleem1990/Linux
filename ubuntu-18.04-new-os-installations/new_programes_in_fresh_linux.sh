2b976a66645cf8a4fcbc84ff2715810b97d8f919
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-03-19 18:58:00 +0500

    updated

--- ubuntu-18.04-new-os-installations/new_programes_in_fresh_linux.sh
+++ ubuntu-18.04-new-os-installations/new_programes_in_fresh_linux.sh
@@ -1,16 +1,23 @@
-sudo apt install -y curl
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+
 sudo apt install -y ipython3
 sudo apt install -y python3-pip
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - && sudo apt-get install apt-transport-https && echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install -y sublime-text
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt update
+sudo apt install sublime-text
 
 sudo apt-get -y install vokoscreen
-
 sudo apt -y install git
-
 sudo apt install -y speedtest-cli
+sudo snap install vlc
+sudo apt-get -y install pinta
+sudo apt install -y nautilus-dropbox
 
 sudo apt-get install -y gdebi
 wget download.opensuse.org/repositories/home:/colomboem/xUbuntu_16.04/amd64/dukto_6.0-1_amd64.deb
@@ -18,24 +25,14 @@ sudo gdebi dukto_6.0-1_amd64.deb
 rm -r dukto_6.0-1_amd64.deb
 
 
-sudo apt-get update && sudo snap install vlc
 
-sudo apt-get -u install pinta
-
-
-wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - && ~/.dropbox-dist/dropboxd
-sudo apt install -y nautilus-dropbox
-
-
-sudo apt install -y apt-transport-https software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
-sudo apt-get update
-sudo apt install -y r-base-core
+sudo apt update
+sudo apt install -y r-base r-base-core r-recommended
 
-sudo apt install tmux
+sudo apt install -y tmux
 
-sudo apt-get install openjdk-11-jdk
+sudo apt-get -y install openjdk-11-jdk
 sudo apt-get -y install r-cran-rjava
 sudo R CMD javareconf
 
@@ -43,15 +40,14 @@ sudo apt-get install -y libcurl4-openssl-dev
 sudo apt-get install -y libxml2-dev
 sudo apt-get install -y libssl-dev
 
-
 sudo Rscript r_essential_packages.R
 
 # to work my alis <copyto>
 sudo apt install xsel
 
 #jupyter
-sudo apt install -y jupyter-notebook
-sudo pip3 install jupyterlab
+sudo apt install -y jupyter-core
+pip3 install jupyterlab
 sudo apt-get install -y jupyter-client
 sudo Rscript R_in_jupyter.R
 
@@ -69,7 +65,23 @@ ipython3 set_startupscript.py
 rm -f set_startupscript.py
 
 # add aliases to .bashrc file
-wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/alias.txt
+wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/alias.sh
 cat alias.txt >> ~/.bashrc
 source ~/.bashrc 
 rm -f alias.txt
+
+
+
+
+sudo apt install nethogs
+sudo apt install tree
+sudo apt install gdebi-core
+sudo apt install ffmpeg
+
+pip3 install youtube-dl
+pip3 install bs4
+pip3 install lxml
+pip3 install selenium
+pip3 install tabulate
+pip3 install pandas
+pip3 install sklearn pandas_profiling clipboard tqdm
