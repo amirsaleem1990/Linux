473c5f31e47e50491295905e698c26339b808fe8
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-03-19 18:23:50 +0500

    updated

--- ubuntu-18.04-new-os-installations/r_essential_packages.R
+++ ubuntu-18.04-new-os-installations/r_essential_packages.R
@@ -26,7 +26,7 @@ install.packages(c(
 	"swirl"
 	))
 
-library(swirl)
+#library(swirl)
 # install_from_swirl("R_Programming")
 # install_course("Getting and Cleaning Data")
 # install_course("Exploratory Data Analysis")
