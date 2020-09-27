8d18d05b55df12a1b072d4b691ede0c6a95e8c86
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-27 18:47:28 +0500

    updated

---
+++ ubuntu-18.04-new-os-installations/keyboard_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir ~/.aa
+cp /home/amir/github/Linux/ubuntu-18.04-new-os-installations/keyboard-layout-creator.zip ~/.aa
+cd ~/.aa
+unzip keyboard-layout-creator.zip
+rm -f keyboard-layout-creator.zip
+cd keboard-layout-creator/
+sudo cp amir_keyobard_linux_arabic_urdu /usr/share/X11/xkb/symbols/
+sudo subl /usr/share/X11/xkb/rules/evdev.xml
+
+
+echo -e "Go to the end of the <layoutList> section (search for </layoutList>). Add the following after the last </layout> tag
+<layout>
+ <configItem>
+   <name>Amir</name>
+   <shortDescription>Amir_personal_keyboard</shortDescription>
+   <description>Amir_keyboard</description>
+   <languageList>
+      <iso639Id>ara</iso639Id>
+      <iso639Id>urd</iso639Id>
+   </languageList>
+ </configItem>
+ <variantList/>
+</layout>"
+
+sudo dpkg-reconfigure xkb-data
+gnome-control-center region
+rm -f ~/.aa || DEL -rf ~/.aa
