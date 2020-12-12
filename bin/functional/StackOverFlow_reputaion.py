6d4b0067fa64c78202d85ba9217bb9f37360d747
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-12-12 12:18:20 +0500

    new

---
+++ bin/functional/StackOverFlow_reputaion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(requests.get("https://stackoverflow.com/users/8875846/amir-saleem").text, "lxml")
+s = f"your StackOverFlow reputation is: {soup.find('strong', {'class' : 'ml6 fc-medium'}).text}"
+s_number = s.split()[-1]
+current_time = str(datetime.now())
+last = open("/home/amir/github/Amir-personal/StackOverFlow_reputaion_record.txt", "r").read().splitlines()[-1].split(",")[1]
+last_number = last.split()[-1]
+if s_number == last_number:
+	print("\nNo increase in reputation, which is:", s_number, "\n")
+	import sys
+	sys.exit(0)
+print("Now:  ", s)
+print("Last: ", last)
+open("/home/amir/github/Amir-personal/StackOverFlow_reputaion_record.txt", "a").write(current_time+","+s+"\n")
