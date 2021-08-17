df5242611fbf825cc6c44157c253c139eff3a675
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-08-17 18:03:34 +0500

    u

--- ubuntu-18.04-new-os-installations/new_programes_in_fresh_linux_NEW.sh
+++ ubuntu-18.04-new-os-installations/new_programes_in_fresh_linux_NEW.sh
@@ -18,8 +18,6 @@ func_(){
 # @install.packages("caret", repos=repo)
 # @install.packages("swirl", repos=repo)
 
-func_ "echo 100 > `locate kbd_backlight`"
-
 func_ "apt-get update"
 func_ "apt-get -y upgrade"
 
@@ -31,6 +29,8 @@ func_ "apt install -y net-tools"
 func_ "apt install -y mlocate"
 func_ "apt install -y testdisk"
 func_ "apt install -y mysql-client-core*"
+
+
 func_ "apt install -y vlc-bin"
 func_ "apt install -y ipython3"
 func_ "apt install -y python3-pip"
@@ -62,6 +62,7 @@ func_ "apt install -y unrar-free"
 func_ "apt install -y dolphin"
 func_ "apt install -y nomacs"
 
+func_ "apt-get install -y mysql-server"
 func_ "apt-get install -y gdebi"
 func_ "apt-get install -y openjdk-11-jdk"
 func_ "apt-get install -y r-cran-rjava"
@@ -70,6 +71,8 @@ func_ "apt-get install -y libxml2-dev"
 func_ "apt-get install -y libssl-dev"
 func_ "apt-get install -y jupyter-client"
 func_ "apt-get install -y gparted"
+
+
 
 func_ "snap install vlc"
 func_ "snap install scrcpy"
@@ -101,18 +104,18 @@ rm -r dukto*.deb
 
 # func_ "apt-get install -y r-base r-base-dev libatlas3-base libopenblas-base"
 # firefox https://cloud.r-project.org/
-apt-get install -y r-base r-base-dev libatlas3-base libopenblas-base  >> /home/amir/results.txT
-if [[ $? != 0 ]]; then 
-	func_ "apt install r-cran-littler"
-if [[ $? != 0 ]]; then read -p "Install R manually, and then pres any key: "; fi
+# apt-get install -y r-base r-base-dev libatlas3-base libopenblas-base  >> /home/amir/results.txT
+# if [[ $? != 0 ]]; then 
+	# func_ "apt install r-cran-littler"
+# if [[ $? != 0 ]]; then read -p "Install R manually, and then pres any key: "; fi
 
-func_ "R CMD javareconf"
-func_ "wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/ubuntu-18.04-new-os-installations/r_essential_packages.R"
-func_ "Rscript r_essential_packages.R"
+# func_ "R CMD javareconf"
+# func_ "wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/ubuntu-18.04-new-os-installations/r_essential_packages.R"
+# func_ "Rscript r_essential_packages.R"
 func_ "pip3 install jupyterlab"
 func_ "pip3 install xlrd==1.2.0"
-func_ "wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/ubuntu-18.04-new-os-installations/R_in_jupyter.R"
-func_ "Rscript R_in_jupyter.R"
+# func_ "wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/ubuntu-18.04-new-os-installations/R_in_jupyter.R"
+# func_ "Rscript R_in_jupyter.R"
 
 
 func_ "wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/ubuntu-18.04-new-os-installations/set_startupscript.py"
@@ -122,15 +125,15 @@ rm -f set_startupscript.py  >> /home/amir/results.txT
 #func_ "wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/ubuntu-18.04-new-os-installations/alias.txt"
 #func_ "wget https://github.com/amirsaleem1990/Linux/blob/master/alias"
 func_ "wget https://raw.githubusercontent.com/amirsaleem1990/Linux/master/alias"
-if [[ $? != 0 ]]; 
-	then echo "cat alias.txt >> ~/.bashrc"
-	exit
-fi
-cp .bashrc{,_backup}; cat .bashrc alias  > xmklk; mv xmklk .bashrc; rm -f xmklk; rm -f alias; source .bashrc
+# if [[ $? != 0 ]]; 
+	#then echo "cat alias.txt >> ~/.bashrc"
+	#exit
+# fi
+# cp .bashrc{,_backup}; cat .bashrc alias  > xmklk; mv xmklk .bashrc; rm -f xmklk; rm -f alias; source .bashrc
 
 
-echo 'PATH="/amir_bin/:$PATH"' >> ~/.bashrc
-source  ~/.bashrc
+#echo 'PATH="/amir_bin/:$PATH"' >> ~/.bashrc
+#source  ~/.bashrc
 
 func_ "pip3 install youtube-dl"
 func_ "pip3 install bs4"
@@ -150,12 +153,12 @@ func_ "pip3 install fpdf"
 func_ "pip3 install pymysql"
 
 
-func_ "curl -fsSL https://get.docker.com -o get-docker.sh"
-func_ "sh get-docker.sh"
+#func_ "curl -fsSL https://get.docker.com -o get-docker.sh"
+#func_ "sh get-docker.sh"
 
-func_ "wget https://raw.githubusercontent.com/amirsaleem1990/git/master/github%20initial%20setup"
-mv 'github initial setup' github_initial_setup
-func_ "bash github_initial_setup"
+#func_ "wget https://raw.githubusercontent.com/amirsaleem1990/git/master/github%20initial%20setup"
+#mv 'github initial setup' github_initial_setup
+#func_ "bash github_initial_setup"
 
 # func_ "mkdir /amir_bin/"
 ln -s  /home/amir/github/Linux/bin/functional/ /amir_bin
@@ -166,10 +169,10 @@ ln -s  /home/amir/github/Linux/bin/functional/ /amir_bin
 
 
 # rstudio
-latest_rstudio_version=`python3 <<< "from bs4 import BeautifulSoup; import requests; print(BeautifulSoup(requests.get('https://www.rstudio.com/products/rstudio/download/#download').text, 'lxml').find('h3', {'id' : 'download'}).text.strip('RStudio Desktop '))"`
-latest_rstudio_download_link="https://download1.rstudio.org/desktop/bionic/amd64/rstudio-$latest_rstudio_version-amd64.deb"
-curl $latest_rstudio_download_link > rstudio-$latest_rstudio_version.deb
-func_ "gdebi -n rstudio*.deb"
+#latest_rstudio_version=`python3 <<< "from bs4 import BeautifulSoup; import requests; print(BeautifulSoup(requests.get('https://www.rstudio.com/products/rstudio/download/#download').text, 'lxml').find('h3', {'id' : 'download'}).text.strip('RStudio Desktop '))"`
+#latest_rstudio_download_link="https://download1.rstudio.org/desktop/bionic/amd64/rstudio-$latest_rstudio_version-amd64.deb"
+#curl $latest_rstudio_download_link > rstudio-$latest_rstudio_version.deb
+#func_ "gdebi -n rstudio*.deb"
 
 
 # dukto
@@ -186,3 +189,9 @@ read ans
 
 # keyboad light ka time 100 seconds kar dya h, by default is ka time 1s hota h
 echo 100s > /sys/devices/platform/dell-laptop/leds/dell\:\:kbd_backlight/stop_timeout
+
+func_ "echo 100 > `locate kbd_backlight`"
+
+
+
+echo "Apply this <'/home/amir/github/Linux/ubuntu-18.04-new-os-installations/Mount\ drive\ in\ linux\ and\ set\ auto-mount\ at\ boot\ -\ Tech\ Knowledge\ Base\ -\ jaytaala.com\ Confluence\ \(8_17_2021\ 1_41_49\ PM\).html'>"
