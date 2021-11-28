61d9a7a3e18e1a595fdaa7e322452a847e0110ce
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-28 19:33:50 +0500

    U

--- ubuntu-18.04-new-os-installations/Set_keyboard_shortcuts.py
+++ ubuntu-18.04-new-os-installations/Set_keyboard_shortcuts.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
+# Copied from:
+    # https://itectec.com/ubuntu/ubuntu-how-to-set-custom-keyboard-shortcuts-from-terminal/
+
 import subprocess
 import sys
+import os
 
 def func(name, command, key_combination):
     # defining keys & strings to be used
@@ -31,30 +36,35 @@ def func(name, command, key_combination):
     cmd3 = 'gsettings set '+subkey1+new+" binding '"+key_combination+"'"
 
     for cmd in [cmd0, cmd1, cmd2, cmd3]:
+        # print(cmd)
         subprocess.call(["/bin/bash", "-c", cmd])
 
-# https://itectec.com/ubuntu/ubuntu-how-to-set-custom-keyboard-shortcuts-from-terminal/
+
+os.system("""gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Alt>t']" """)
+os.system("""gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']" """)
+os.system("""gsettings set org.gnome.settings-daemon.plugins.media-keys www "['<Super>w']" """)
+
+
 
 lst = [
-['system monitor',            'gnome-system-monitor',                '<Alt>p'              ],
-['pinta',                     'pinta',                               '<Super>]'            ],
-['screen_shot_selected_area', '/amir_bin/screen_shot_selected_area', '<Alt>a'              ],
-['screen_shot_full_window',   '/amir_bin/screen_shot_full_window',   '<Alt>s'              ],
-['touch_on_off',              '/amir_bin/touch_on_off',              '<Shift><Control><f9>'],
-['Paste',                     '/home/amir/Paste',                    '<Control>1'          ],
-['Mn',                        '/usr/bin/xdotool key space',          '<Shift><Control>m'   ],
-['Night_light_modify shiny',  '/amir_bin/Night_light_modify +' ,     '<Control><Alt>+'     ],
-['Night_light_modify dark',   '/amir_bin/Night_light_modify -' ,     '<Control><Alt>-'     ],
-['night_light',               '/amir_bin/night_light',               '<Control><Alt>n'     ],
-['namaz',                     '/amir_bin/namaz',                     '<Shift><Control>n'   ]
+['system monitor',            'gnome-system-monitor',                '<Alt>p'              ], # ALT + p 
+['pinta',                     'pinta',                               '<Super>bracketright' ], # Super + ]
+['screen_shot_selected_area', '/amir_bin/screen_shot_selected_area', '<Alt>a'              ], # ALT + a
+['screen_shot_full_window',   '/amir_bin/screen_shot_full_window',   '<Alt>s'              ], # ALT + s
+['touch_on_off',              '/amir_bin/touch_on_off',              '<Primary><Shift>F9'  ], # SHFT + CTRL + F9
+['Paste',                     '/home/amir/Paste',                    '<Control>1'          ], # CTRL + 1
+['Mn',                        '/usr/bin/xdotool key space',          '<Shift><Control>m'   ], # SHIFT + CTRL + m
+['Night_light_modify shiny',  '/amir_bin/Night_light_modify +' ,     '<Primary><Alt>equal' ], # CTRL + ALT + =
+['Night_light_modify dark',   '/amir_bin/Night_light_modify -' ,     '<Primary><Alt>minus' ], # CTRL + ATL + -
+['night_light',               '/amir_bin/night_light',               '<Control><Alt>n'     ], # CTRL + ALT + n
+['namaz',                     '/amir_bin/namaz',                     '<Shift><Control>n'   ]  # SHIFT + CTRL + n
 ]
 
 for i in lst:
-    name, command, key_combination = i
-    func(name, command, key_combination)
+    func(*i)
 
-#python3 script.py '<name>' '<command>' '<key_combination>'
-#python3 script.py 'open gedit' 'gedit' '<Alt>7'
+#python3 this_script.py '<name>' '<command>' '<key_combination>'
+#python3 this_script.py 'open gedit' 'gedit' '<Alt>7'
 
 #Super key:                 <Super>
 #Control key:               <Primary> or <Control>
