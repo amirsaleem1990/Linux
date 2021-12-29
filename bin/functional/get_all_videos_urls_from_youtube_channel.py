bee2d591396b738acf8ab9154101776baed27c8d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-12-29 19:07:38 +0500

    new

---
+++ bin/functional/get_all_videos_urls_from_youtube_channel.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import os
+import sys
+from get_soup_object_using_selenium import get_soup_object_using_selenium
+
+try:
+	channel_url = sys.argv[1]
+	if not 'youtube' in channel_url:
+		channel_url = input("Enter channel url: ")
+except:
+	channel_url = input("Enter channel url: ")
+x = get_soup_object_using_selenium(channel_url)
+urls = ['https://www.youtube.com'+i for i in x[0] if i.startswith("/watch?v=")]
+
+file_name = "mp4_links.txt"
+while os.path.exists(file_name):
+	file_name = input(f"\nThe {file_name} is already exists, please enter another name: ")
+open(file_name, 'w').write("\n".join(urls))
+print(f"Saved {len(urls)} urls in {file_name}")
