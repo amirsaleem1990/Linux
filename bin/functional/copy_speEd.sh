4f0b919808b2f2a82f1b352183031c9aace9a07d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2022-05-15 12:45:12 +0500

    updated

--- bin/functional/copy_speEd.sh
+++ bin/functional/copy_speEd.sh
@@ -4,10 +4,10 @@ if [[ -z $1 ]]; then
 else
 	sleep_secs=$1
 fi
-size=$(du -s -BM | cut -d'M' -f1)
+size=$(du -s -BM 2>/dev/null | cut -d'M' -f1)
 while :; do
-	current_size=$(du -s -BM | cut -d'M' -f1)
-	echo -e "$(date +%T)\t$current_size MB\t$(echo $current_size-$size|bc -l) MB\t$(find . | wc -l)"
+	current_size=$(du -s -BM 2>/dev/null | cut -d'M' -f1)
+	echo -e "$(date +%T)\t$current_size MB\t$(echo $current_size-$size|bc -l) MB\t$(find . 2>/dev/null| wc -l)"
 	size=$current_size
 	sleep $sleep_secs
 done
