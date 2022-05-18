794b2ea2e250fc753ff221c23b0656cddabd527e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2022-05-18 18:43:18 +0500

    record to /home/amir/.copyt_speed.txt as well

--- bin/functional/copy_speEd.sh
+++ bin/functional/copy_speEd.sh
@@ -7,7 +7,9 @@ fi
 size=$(du -s -BM 2>/dev/null | cut -d'M' -f1)
 while :; do
 	current_size=$(du -s -BM 2>/dev/null | cut -d'M' -f1)
-	echo -e "$(date +%T)\t$current_size MB\t$(echo $current_size-$size|bc -l) MB\t$(find . 2>/dev/null| wc -l)"
+	echo -e "$(date +%T)\t$current_size MB\t$(echo $current_size-$size|bc -l) MB\t$(find . 2>/dev/null| wc -l)" | tee -a /home/amir/.copyt_speed.txt
 	size=$current_size
 	sleep $sleep_secs
 done
+
+eche -e "\nSee /home/amir/.copyt_speed.txt\n"
