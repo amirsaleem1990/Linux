98a07c25b2f65966d23a19b4ffc8a1528d7bb171
amirsaleem1990 <amirsaleem1990@hotmail.com>
2022-06-06 17:31:25 +0500

    u

--- bin/functional/internet_SPEED.sh
+++ bin/functional/internet_SPEED.sh
@@ -8,32 +8,34 @@ if [[ -z $secs_to_sleep ]]; then
 	secs_to_sleep=0
 fi
 func() {
-var_=$1
-# echo -e "var:\t$var_"
-lenght=${#var_}
-# echo -e "lenght:\t$lenght"
-provided_lenght=$2
-if [[ $lenght == $provided_lenght ]]; then
-echo $var_
-else
-add_zeros=$(echo $provided_lenght-$lenght-1|bc -l)
-# echo -e "add_zeros:\t$add_zeros"
-# echo $(echo -n  "print($add_zeros*'_')" | python3)$var_
-for i in $(seq $add_zeros) ; do 
-	echo -n ' '
-done
-echo $var_
-echo
-fi
+	var_=$1
+	# echo -e "var:\t$var_"
+	lenght=${#var_}
+	# echo -e "lenght:\t$lenght"
+	provided_lenght=$2
+	if [[ $lenght == $provided_lenght ]]; then
+		echo $var_
+	else
+		add_zeros=$(echo $provided_lenght-$lenght-1|bc -l)
+		# echo -e "add_zeros:\t$add_zeros"
+		# echo $(echo -n  "print($add_zeros*'_')" | python3)$var_
+		for i in $(seq $add_zeros) ; do 
+			echo -n ' '
+		done
+		echo $var_
+		echo
+	fi
 }
 
-echo 'Speed|Total|Avg|Time|current_time|Downloaded' >  '/home/amir/.speed_hist.csv'
+# echo 'Speed|Total|Avg|Time|current_time|Downloaded' >  '/home/amir/.speed_hist.csv'
+/usr/bin/rm '/home/amir/.speed_hist.csv' 2>/dev/null
 
 plot() {
 	echo '
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv("/home/amir/.speed_hist.csv", sep="|", index_col=None)
+df.columns = ["Speed", "Total", "Avg", "Time", "current_time", "Downloaded"]
 df.Speed = df.Speed.str.strip("Speed:").str.strip().str.strip("K").astype(float)
 d,h,m,s = [i.zfill(2) for i in df.Time.iloc[-1].strip().split(":")]
 df.set_index("current_time").Speed.plot(title=f"DD:HH:MM:SS\n  {d}:{h}:{m}:{s}")
@@ -43,11 +45,61 @@ plt.axhline(df.Speed.quantile(0.75), color="green");
 plt.show()
 ' | python3
 }
+
+live_plot(){
+echo '
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import StringIO
+from collections import deque
+import os
+
+from matplotlib import style
+style.use("fivethirtyeight")
+
+def animate(i):
+	if not os.path.exists("/home/amir/.speed_hist.csv"):
+		return
+	rows_count = len(open("/home/amir/.speed_hist.csv", "r").read().splitlines()) -1
+	if  rows_count < 1:
+		return
+	
+	# df = pd.read_csv("/home/amir/.speed_hist.csv", sep="|", index_col=None).iloc[-60:]
+
+	last_n_rows = deque(open("/home/amir/.speed_hist.csv", "r"), 300 if rows_count > 300 else rows_count) # load only last 300 lines
+	df = pd.read_csv(StringIO("".join(last_n_rows)), header=None, sep="|")
+	df.columns = ["Speed", "Total", "Avg", "Time", "current_time", "Downloaded"]
+	df.Speed = df.Speed.str.strip("Speed:").str.strip().str.strip("K").astype(float)
+	d,h,m,s = [i.zfill(2) for i in df.Time.iloc[-1].strip().split(":")]
+	ax1.clear()
+	ax1.plot(df.current_time, df.Speed)#, title=f"DD:HH:MM:SS\n  {d}:{h}:{m}:{s}")
+	ax1.axhline(df.Speed.mean(), color="red");
+	ax1.axhline(df.Speed.quantile(0.25), color="green");
+	ax1.axhline(df.Speed.quantile(0.75), color="green");
+	plt.title(f"DD:HH:MM:SS\n  {d}:{h}:{m}:{s}")
+	plt.xticks(rotation=90)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
+' | python3
+}
+
 x=-1
+
+ifstat 1s 1 | grep wlxd037459bf2ea
+if [[ $? -eq 0 ]]; then
+	wifi_chip=wlxd037459bf2ea
+else
+	wifi_chip=wlp3s0
+fi
+
 main () {
 	let "n++"
-	current=$(ifstat -i wlp3s0 1s 1 | awk 'NR==3 {print $1}')
-	# current=$(ifstat -i wlxd037459bf2ea 1s 1 | awk 'NR==3 {print $1}')
+	current=$(ifstat -i $wifi_chip 1s 1 | awk 'NR==3 {print $1}')
 	current_=$(func $current 8)K
 
 	total=$(echo $current + $total|bc)
@@ -85,7 +137,15 @@ control_c() {
 
 trap control_c SIGINT
 
+
+
+(live_plot &)
 while true ; do 
    main
    sleep $secs_to_sleep
 done
+
+
+
+
+
