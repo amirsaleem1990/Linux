79dc78b857e576611495f7df1172475bde1bcd45
amirsaleem1990 <amirsaleem1990@hotmail.com>
2022-08-30 13:27:59 +0500

    BEFORE: jo files 50MB sy bari hoti hen un ki list aa jati thi, or prompt me question hota tha k kya aap ny in files ko .gitignore me dala hwa h? agar user 'y' kehta tha to assuming k us ny sari listed files ko one by one check kar lya h; script sab ko push kar deti thi, since ye list kafi bari hoti ja rahi thi to one by one sab files ko check karna bohot mushkil ho raha tha is lye me ny user prompt khatam kar dya, or khud hi check kar lya. AFTER: ab jo file 50MB sy bari ho gi; script us ko check kary gi, agar wo file .gitignore me ho gi to script us ko ignore kar dy gi, or agar wo big file .gitignore me nahi ho gi to msg print ho ga or script terminate ho jay gi.

--- bin/functional/check_github_files_size.sh
+++ bin/functional/check_github_files_size.sh
@@ -21,38 +21,48 @@ echo -e "\n\nThese big files are not included in the git_ignore_file\n"
 n=0
 for i in $(find . -size  +51199k | grep -v  '^.\/\.' | grep -v '\.pack$\|lfd-projects') ; do  
 	file_name=$(basename $i)
-	git_ignore_file=/home/amir/github/$(echo $i|  sed 's/^\.\///g' | cut -d/ -f1)/.gitignore
+	repo_name=$(echo $i | cut -d/ -f2)
+
+	cd /home/amir/github/"$repo_name"
+
+	# git_ignore_file=/home/amir/github/$(echo $i|  sed 's/^\.\///g' | cut -d/ -f1)/.gitignore
 	big_file_name=$(echo $i | rev | cut -d/ -f1 | rev)
 	
-	grep $big_file_name $git_ignore_file >/dev/null
+	# grep $big_file_name $git_ignore_file >/dev/null
 
-	if [[ $? -ne 0 ]]; then
-		let "n++"
-	else
-		continue
-	fi
+	# if [[ $? -eq 0 ]]; then
+	# 	continue
+	# fi
+
 
 	# git_lfs_files=$(cat `find /home/amir/github -name ".gitattributes"` | grep $file_name | wc -l)
 	# if [[ $git_lfs_files > 0 ]]; then
 	# continue
 	# fi
+
+	git check-ignore "/home/amir/github/$i" >/dev/null
+	if [[ $? -eq 0 ]]; then
+		continue
+	fi
+
+	let "n++"
+
 	file_greater_than_one=$(( $file_greater_than_one + 1 )) 
-	du -sh $i >> /home/amir/.GIT_FILES_GRATER_THAN_50_MB
-	du -sh $i
+	du -sh "/home/amir/github/$i" >> /home/amir/.GIT_FILES_GRATER_THAN_50_MB
+	du -sh "/home/amir/github/$i"
 done
 
 if [[ $file_greater_than_one > 0 ]]; then
 	IFS=$'\n'
 	RED='\033[0;31m'
 	NC='\033[0m' # No Color
-	echo -e "\n\n${RED}These files are > 50MB, we need files < 50MB\nPrepare these files and try again${NC}\n\n"
-	echo -e "\n\nThese files are > 50MB, we need files < 50MB\nPrepare these files and try again\n\n" >> /home/amir/.GIT_FILES_GRATER_THAN_50_MB
-
-	#subl /home/amir/.GIT_FILES_GRATER_THAN_50_MB
-	echo -e "\nAre these files in .gitignore, and you're sure that they can't be pushed? [y|n] "
-	read ans
-	if [[ $ans == "y" ]]; then
-		exit
-	fi
+	echo -e "\n\n${RED}These files are > 50MB, we need files < 50MB\nAdd this/theese file/s to .gitignore or move them from repository, and try again${NC}\n\n"
+	# # echo -e "\n\nThis/these file/s is/are > 50MB, we need files < 50MB\nAdd this/theese file/s to .gitignore or move them from repository, and try again\n\n" >> /home/amir/.GIT_FILES_GRATER_THAN_50_MB
+	# # subl /home/amir/.GIT_FILES_GRATER_THAN_50_MB
+	# echo -e "\nAre these files in .gitignore, and you're sure that they can't be pushed? [y|n] "
+	# read ans
+	# if [[ $ans == "y" ]]; then
+	# 	exit
+	# fi
 	exit 2
 fi
