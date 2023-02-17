a86b84ace2637d6c79666e9430cba98599265aa6
amirsaleem1990 <amirsaleem1990@hotmail.com>
2023-02-17 17:01:13 +0500

    new

---
+++ bin/functional/pylint_amir.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+pylint --msg-template='{msg_id}:{line:3d}:{column}:{msg}' "$1" | grep --color=auto "^[A-Za-z-]" | head -n -2 | sort -k1,4 -s > /tmp/xx
+cat /tmp/xx | cut -d: -f1 | sort -u | sort > unique_codes
+cat /tmp/xx | cut -d: -f1 | sort | uniq -c > distribution
+
+cat /tmp/xx
+
+python3 <<< '
+import pandas as pd
+import json
+def func(line):
+    count, code = line.strip().split()
+    return code, count
+unique_codes = open("unique_codes", "r").read().splitlines()
+distribution = open("distribution", "r").read().splitlines()
+pylint_codes = json.load(open("/home/amir/github/Linux/pylint_codes.txt"))
+selected_pylint_codes = {k: v for k,v in pylint_codes.items() if k in unique_codes}
+distribution = dict(map(func, distribution))
+selected_distribution = {k: int(v) for k,v in distribution.items() if k in unique_codes}
+print(
+    pd.DataFrame.from_dict(
+        selected_distribution, 
+        orient="index", 
+        columns=["Count"]
+    )
+	.rename_axis("Code")
+	.merge(
+        pd.DataFrame.from_dict(
+            selected_pylint_codes, 
+            orient="index", 
+            columns=["Error"]
+        ).rename_axis("Code"), 
+        left_index=True, 
+        right_index=True
+    )
+    .reset_index()
+    .sort_values(
+        by="Code"
+    )
+    .to_string()
+)
+'
