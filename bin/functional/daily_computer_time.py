ab2993564b7b183b5789e8115601fb2f35af2326
amirsaleem1990 <amirsaleem1990@hotmail.com>
2023-07-01 20:38:05 +0500

    new

---
+++ bin/functional/daily_computer_time.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from amir_analysis_functions import connect_to_my_db, is_computer_locked
+import datetime
+
+def daily_computer_time():
+	DELAY_MINUTES=10
+	conn = connect_to_my_db()
+	cursor = conn.cursor()
+	today = str(datetime.date.today())
+	current_time = str(datetime.datetime.now().time()).split(".")[0]
+	#cursor.execute("PRAGMA journal_mode=wal")
+	cursor.execute(
+		"INSERT INTO daily_computer_use_duration (date, delay_minutes, is_computer_locked, current_time) VALUES (?, ?, ?, ?)", 
+		(today, DELAY_MINUTES, int(is_computer_locked()), current_time)
+		)
+	conn.commit()
+	conn.close()
+daily_computer_time()
