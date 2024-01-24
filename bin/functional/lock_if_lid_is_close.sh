8956f6c65b213a4b10dfacabcc2e6765fdf07066
amirsaleem1990 <amirsaleem1990@hotmail.com>
2024-01-24 21:51:27 +0500

    updated

--- bin/functional/lock_if_lid_is_close.sh
+++ bin/functional/lock_if_lid_is_close.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/bash
+file_name=$(ls /proc/acpi/button/lid/*/state)
+# file_name=$(ls /proc/acpi/button/lid/LID0/state)
+# test -e "$file_name"
+# if [[ $? -ne 0 ]]; then
+#     file_name=/proc/acpi/button/lid/LID/state
+#     test -e "$file_name"
+#     if [[ $? -ne 0 ]]; then
+#         echo -e "\nNeither of '/proc/acpi/button/lid/LID/state' or '/proc/acpi/button/lid/LID0/state' exist."
+#         return 1
+#     fi
+# fi
+
 while true; do
-    if [ $(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}') == "closed" ]; then
+    if [ $(cat "$file_name" | awk '{print $2}') == "closed" ]; then
         # gnome-screensaver-command --lock
         xdg-screensaver lock
     fi
-    sleep 0.3s
+    sleep 1s
 done
