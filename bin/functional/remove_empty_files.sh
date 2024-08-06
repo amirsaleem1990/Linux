35037d93f83a46f1094251ab8f1d70503dfe5a2e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2024-08-06 10:38:58 +0500

    Updated

--- bin/functional/remove_empty_files.sh
+++ bin/functional/remove_empty_files.sh
@@ -5,14 +5,30 @@ starting_qty=$qty
 echo -e "$starting_qty empty files/folders"
 
 n=0
-while [ $qty -gt 0 ]; do
+while true; do
 	if [[ $n > 10 ]]; then
 		break
 	fi
 	let "n++"
 	IFS=$'\n'
-	/amir_bin/DEL -rf $(find . -empty)
-	qty=$(find . -empty | wc -l)
+	empty_files_and_folders_count=$(find . -empty | wc -l)
+
+	if [[ $empty_files_and_folders_count -eq 0 ]]; then
+		exit
+	elif [[ $empty_files_and_folders_count -eq 1 ]]; then
+		if [[ $(find . -empty) == "." ]] ; then
+			exit
+		fi
+	fi
+	echo -e "\n\nEmpty Files/Folders list ......\n"
+	IFS=$'\n'
+	/amir_bin/dt $(find . -empty)
+	echo -e "\nWe are going to remove above empty files/folders, do you agree? [yes|no]"
+	read response
+	if [[ $response == "yes" ]]; then
+		/amir_bin/DEL -rf $(find . -empty)
+	fi
+	empty_files_and_folders_count=$(find . -empty | wc -l)
+	echo -e "\n>>>>>>>>>>>>> $empty_files_and_folders_count empty files/folders still exist"
 done
 
-echo -e "$qty empty files/folders"
