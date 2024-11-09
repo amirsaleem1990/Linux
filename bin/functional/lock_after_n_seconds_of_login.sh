2dc4aab8e174f28b803d285c3d1ad4f020fb5bb2
amirsaleem1990 <amirsaleem1990@hotmail.com>
2024-11-09 22:02:11 +0500

    u

--- bin/functional/lock_after_n_seconds_of_login.sh
+++ bin/functional/lock_after_n_seconds_of_login.sh
@@ -1,20 +1,21 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 
 #/home/amir/.config/autostart/lock-screen-after-n-seconds-of-login.desktop
 
-
 file_name=/home/amir/github/Amir-personal/lock-screen-after-n-seconds-of-login.txt
 echo true > "$file_name"
+sleep 10s
 
 while true; do
-    computer_screen_locked=$(is_computer_screen_locked)
-    if [ $x == true ] ; then 
+    is_computer_screen_locked=$(/amir_bin/is_computer_screen_locked.py)    
+    if [[ "$is_computer_screen_locked" == "true" ]] ; then 
+        sleep 2
         echo true > "$file_name"
-    else:
-        x=$(cat "$file_name")
-        if [ $x == true ] ; then 
+    else
+        x=$(cat "$file_name")        
+        if [[ "$x" == "true" ]] ; then 
             /usr/bin/xdg-screensaver lock
         fi
     fi
-    sleep 10s
+    sleep 5
 done
