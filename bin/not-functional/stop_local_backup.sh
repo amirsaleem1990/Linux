4b3baf192453cd01d172745e9269fde56bb306e7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2025-11-27 20:10:05 +0500

    new

---
+++ bin/not-functional/stop_local_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+t=`cat /home/amir/.Local_backup_permision`
+if [[ $t == False ]]; then
+	echo "Already Stoped before"
+	exit 1
+else
+	echo -en  "Do you want to STOP local backup?\n[y|n]:  "
+	read ans
+	if [[ $ans != 'y' ]]; then
+		exit 1
+	fi
+	echo False > /home/amir/.Local_backup_permision
+	if [[ $? == 0 ]]; then
+		echo "Local backup permision set to <FALSE>."
+	fi
+	echo 
+
+	next_local_backup_time
+fi
